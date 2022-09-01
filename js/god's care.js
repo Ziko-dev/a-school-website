f9cffcd12c665ff40321c5839c72eb71f8866d79
ziko dev <chukwumanwadiogbu100@gmail.com>
2022-09-01 02:04:37 +0100

    included swiper js

--- js/god's care.js
+++ js/god's care.js
@@ -5,29 +5,17 @@ toggleButton.addEventListener('click', () => {
 navBar.classList.toggle('active')
 })
 
-var swiper = new Swiper(".slide-content", {
-    slidesPerView: 1,
-    centeredSlides: false,
-    slidesPerGroupSkip: 1,
-    grabCursor: true,
-    keyboard: {
-      enabled: true,
-    },
-    breakpoints: {
-      769: {
-        slidesPerView: 2,
-        slidesPerGroup: 2,
-      },
-    },
-    scrollbar: {
-      el: ".swiper-scrollbar",
-    },
-    navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
-    },
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
-    },
-  });
+
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 1,
+  spaceBetween: 30,
+  loop: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
